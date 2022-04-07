226710cf3427d3d65571cb5f44872fdf09ef7792
Anthony Hu <anth.hu@gmail.com>
2022-04-07 17:58:00 +0100

    Train with head learning rate x10

--- configs/swin/upernet_swin_tiny_patch4_window7_512x512_160k_ade20k.py
+++ configs/swin/upernet_swin_tiny_patch4_window7_512x512_160k_ade20k.py
@@ -27,7 +27,9 @@ model = dict(
 optimizer = dict(_delete_=True, type='AdamW', lr=0.00006, betas=(0.9, 0.999), weight_decay=0.01,
                  paramwise_cfg=dict(custom_keys={'absolute_pos_embed': dict(decay_mult=0.),
                                                  'relative_position_bias_table': dict(decay_mult=0.),
-                                                 'norm': dict(decay_mult=0.)}))
+                                                 'norm': dict(decay_mult=0.),
+                                                 'head': dict(lr_mult=10.),
+                                                 }))
 
 lr_config = dict(_delete_=True, policy='poly',
                  warmup='linear',
